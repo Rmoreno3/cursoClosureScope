5a6f0dd5cc3e1ff35cc2f9188b0395a2d27806d4
Rmoreno3 <reynaldomoreno173@gmail.com>
2021-03-27 12:15:42 -0400

    debug hositing

---
+++ src/hoisting/index.js
@@ -0,0 +1,18 @@
+a = 2;
+var a;
+console.log(a);
+
+var b = 5;
+console.log(b);
+
+nameOfDog('chikitin');
+function nameOfDog (name) {
+  console.log(name);
+};
+
+const helloWorld = () => {
+  debugger
+  globalVar = "I'm global"; 
+} 
+
+helloWorld();
